d17d7fe19a9f65f10e53c1eafbbb72b9a9e565cc
Olena Horovenko <elena.gorovenko@gmail.com>
2024-05-21 19:26:16 +0200

    App and HomePage start

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
@@ -8,26 +7,7 @@ export const App = () => (
   <>
     <Header />
     <Outlet />
-    <Footer />
   </>
 );
-
-const App = () => {
-  const { t, i18n } = useTranslation(['translation', 'common']);
-
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-
-  return (
-    <div>
-      <h1>{t('welcome')}</h1>
-      <p>{t('description')}</p>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button onClick={() => changeLanguage('ua')}>Українська</button>
-      <button onClick={() => changeLanguage('cz')}>Čeština</button>
-    </div>
-  );
-};
 
 export default App;
